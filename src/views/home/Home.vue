941130e0f02dd70f30f632e14052e06c6638dad3
weizhou <123456>
2021-06-30 11:24:29 +0800

    '实现首页走马灯'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,33 @@
 <template>
 <div>
   <nav-bar id="home-nav-bar"><div slot="center">购物街</div></nav-bar>
+  <home-swiper :banners="banner"></home-swiper>
 </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childs/HomeSwiper'
+
+import { getHomeMutiData } from '../../network/home.js'
+
 export default {
   name: 'Home',
   components: {
-    NavBar
+    NavBar,
+    HomeSwiper
+  },
+  data () {
+    return {
+      banner: [],
+      recommond: []
+    }
+  },
+  created () {
+    getHomeMutiData().then((res) => {
+      this.banner = res.data.banner.list
+      this.recommond = res.data.recommend.list
+    })
   }
 }
 </script>
---
+++ src/views/home/childs/HomeSwiper.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-carousel trigger="click" class="el-carousel" height="195px">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+// import { SwiperItem, Swiper } from 'components/common/swiper'
+
+export default {
+  name: 'HomeSwiper',
+  components: {
+    // SwiperItem,
+    // Swiper
+  },
+  props: {
+    banners: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*.el-carousel {*/
+/*  !*width: 100%;*!*/
+/*  display: flex;*/
+/*}*/
+.el-carousel img {
+  width: 100%;
+}
+</style>
